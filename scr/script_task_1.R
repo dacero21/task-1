fd0a52468cbd4cbd5ff9fd621ff2f8cb92f46e3f
eduard-martinez <edfemagonza@gmail.com>
2021-04-06 10:09:00 -0500

    Nota: 5.0

--- scr/script_task_1.R
+++ scr/script_task_1.R
@@ -13,6 +13,7 @@ pacman::p_load(tidyverse,readxl,haven,xlsx) #Cargar los paquetes necesarios
 vector_1_100=c(1:100) #Crear el vector vector_1_100 con los numeros del uno al cien.
 vector_impar=seq(1,99,by = 2) #Crear el vector vector_impar cocon los números impares del uno al 99.
 vector_par=vector_1_100[-vector_impar] #Crear el vector vector_par a partir de los primeros dos vectores.
+cat("Muy bien!")
 
 #2. Limpiar una base de datos. 
 
@@ -20,6 +21,7 @@ cultivos <- read_excel("data/input/cultivos.xlsx",range = "A9:Y362") # Importar 
 cultivos = cultivos[-c(which(is.na(cultivos$DEPARTAMENTO))),] #Eliminar las filas de totales pues están lelnas de NA
 cultivos=gather(cultivos,"Año","Hectareas de coca",5:25) #Pivotar la base de datos para que tenga un formato long
 cultivos$`hectareas de coca`=replace(cultivos$`hectareas de coca`,which(is.na(cultivos$`hectareas de coca`)),0) #Remplazar los valores de NA por ceros en la columna de hectareas de coca
+cat("Muy bien!")
 
 #3. GEIH
 
@@ -28,14 +30,13 @@ cultivos$`hectareas de coca`=replace(cultivos$`hectareas de coca`,which(is.na(cu
 caracteristicas=readRDS("data/input/2019/Cabecera - Caracteristicas generales (Personas).rds") #Importar la base de caracteristicas
 ocupados=readRDS("data/input/2019/Cabecera - Ocupados.rds") #Importar la base de ocupados
 base_final=merge(caracteristicas,ocupados,by = c("directorio","secuencia_p","orden")) #Unificar las bases caracteristicas y ocupados
+cat("Me gusta más la función left_join(), si quieren después discutimos las ventajas")
 
 #3.2 Descriptivas
 #Tabla
 base_stats=dplyr::select(base_final,ESC, P6020, P6160, P6170, P6040, P6050 ) #Se crea un subset con las variables de interes
 print(summary(base_stats)) #Imprime estadisticas de las variables escogidas
-
-
-
+cat("Muy bien lo del summary() ya que todas las variables son numericas")
 
 #Graficas
 
